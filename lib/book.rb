a3f78ddbdb81ce91925a2f42e1f77a263426efa2
GeoffreyMokua <geoffrey.nyakundi@student.moringaschool.com>
2022-10-20 22:16:24 +0300

    completed assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,30 @@
 
+class Book
+    attr_reader :author, :page_count, :genre
 
+    def initialize(title)
+        @title = title
+    end
+    def title
+        @title
+    end
+
+    def author=(author)
+        @author = author
+    end
+
+    def page_count=(num)
+        @page_count = num
+    end
+
+
+    def genre=(thriller)
+        @genre = thriller
+    end
+
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+
+    end
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,36 @@
 # Make your shoe class here!
+
+class Shoe
+ attr_reader :color, :size, :material, :condition
+
+def initialize(brand)
+    @brand = brand
+end
+
+def brand
+    @brand
+    end
+
+    def color=(color)
+        @color = color
+    end
+
+    def size=(size)
+        @size = size
+    end
+
+    def material=(material)
+        @material = material
+    end
+
+    def condition=(condition)
+        @condition = condition
+    end
+
+    def cobble
+    puts "Your shoe is as good as new!"
+     @condition = "new"
+    end
+
+
+end
